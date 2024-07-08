06870f8a1363b1df98c449d6bf84025d3d5f2dc0
Mohammed Sameer Ahmad <md.sameerahmed8@gmail.com>
2024-07-08 07:34:57 +0530

    feat: initial setup with nav host and added fragment for testing navigation component dependency.

---
+++ app/src/main/java/com/sameerapps/navigationcomponentexample/FirstFragment.kt
@@ -0,0 +1,38 @@
+package com.sameerapps.navigationcomponentexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment )
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FirstFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
